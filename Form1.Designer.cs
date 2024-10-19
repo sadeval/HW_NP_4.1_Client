dd1e55833257b56d22bc24af97c8b14d8c564a88
sadeval <162736837+sadeval@users.noreply.github.com>
2024-10-19 16:26:42 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System;
+
+namespace TicTacToeClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 420); 
+            this.Text = "Крестики-Нолики";
+
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.btnRestart = new System.Windows.Forms.Button();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.btnResign = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.cmbGameMode = new System.Windows.Forms.ComboBox(); 
+
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 3;
+            this.tableLayoutPanel.RowCount = 3;
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Top; 
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.Size = new System.Drawing.Size(450, 450);
+            this.tableLayoutPanel.TabIndex = 0;
+
+            // Создание кнопок для игрового поля
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Button button = new Button();
+                    button.Size = new Size(100, 100);
+                    button.Font = new Font("Arial", 24, FontStyle.Bold);
+                    button.Click += new EventHandler(Button_Click);
+                    this.tableLayoutPanel.Controls.Add(button, j, i);
+                }
+            }
+
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.Location = new System.Drawing.Point(350, 15);
+            this.btnRestart.Size = new System.Drawing.Size(100, 50);
+            this.btnRestart.Text = "Перезапустить";
+            this.btnRestart.Click += new EventHandler(BtnRestart_Click);
+            this.Controls.Add(this.btnRestart);
+
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(350, 75);
+            this.btnDraw.Size = new System.Drawing.Size(100, 50);
+            this.btnDraw.Text = "Ничья";
+            this.btnDraw.Click += new EventHandler(BtnDraw_Click);
+            this.Controls.Add(this.btnDraw);
+
+            // 
+            // btnResign
+            // 
+            this.btnResign.Location = new System.Drawing.Point(350, 135);
+            this.btnResign.Size = new System.Drawing.Size(100, 50);
+            this.btnResign.Text = "Сдаться";
+            this.btnResign.Click += new EventHandler(BtnResign_Click);
+            this.Controls.Add(this.btnResign);
+
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(100, 380);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 20);
+            this.Controls.Add(this.lblStatus);
+
+            // 
+            // cmbGameMode
+            // 
+            this.cmbGameMode.Items.AddRange(new object[] {
+        "Человек-Человек",
+        "Человек-Компьютер",
+        "Компьютер-Компьютер"});
+            this.cmbGameMode.DropDownStyle = ComboBoxStyle.DropDownList;
+            this.cmbGameMode.Location = new System.Drawing.Point(10, 330);
+            this.cmbGameMode.Name = "cmbGameMode";
+            this.cmbGameMode.Size = new System.Drawing.Size(200, 28);
+            this.cmbGameMode.TabIndex = 1;
+            this.Controls.Add(this.cmbGameMode);
+
+            // 
+            // MainForm
+            // 
+            this.Controls.Add(this.tableLayoutPanel);
+            this.Name = "MainForm";
+            this.Text = "Крестики-Нолики";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(MainForm_FormClosing);
+        }
+
+
+        #endregion
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TicTacToeClient
+{
+    public partial class MainForm : Form
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+
+        private char currentPlayer = 'X'; 
+        private char[,] gameBoard = new char[3, 3]; 
+        private bool gameActive = true; 
+        private string gameMode = "";
+        private System.Windows.Forms.Button btnRestart;
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Button btnResign;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.ComboBox cmbGameMode;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectButton_Click(object sender, EventArgs e)
+        {
+            client = new TcpClient("127.0.0.1", 5500);
+            stream = client.GetStream();
+            SendGameMode();
+            WaitForGameStart();
+        }
+
+        private void SendGameMode()
+        {
+            string selectedMode = cmbGameMode.SelectedItem.ToString();
+            byte[] data = Encoding.UTF8.GetBytes(selectedMode);
+            stream.Write(data, 0, data.Length);
+        }
+        private void WaitForGameStart()
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            gameMode = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            MessageBox.Show($"Игра в режиме: {gameMode}");
+            gameActive = true; 
+        }
+
+        private void SendMove(int x, int y)
+        {
+            byte[] data = new byte[2] { (byte)(x + '0'), (byte)(y + '0') };
+            stream.Write(data, 0, data.Length);
+            ReadBoardState();
+        }
+
+        private void ReadBoardState()
+        {
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            string boardState = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            UpdateBoard(boardState);
+        }
+
+        private void UpdateBoard(string boardState)
+        {
+            string[] rows = boardState.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    char mark = rows[i][j];
+                    Button button = tableLayoutPanel.GetControlFromPosition(j, i) as Button;
+                    if (button != null)
+                    {
+                        button.Text = mark == ' ' ? "" : mark.ToString();
+                    }
+                }
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            client?.Close();
+        }
+
+        
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if (!gameActive) return; 
+
+            Button clickedButton = sender as Button;
+
+            if (clickedButton.Text != "")
+            {
+                MessageBox.Show("Эта ячейка уже занята!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            clickedButton.Text = currentPlayer.ToString();
+            int row = tableLayoutPanel.GetRow(clickedButton);
+            int col = tableLayoutPanel.GetColumn(clickedButton);
+            gameBoard[row, col] = currentPlayer;
+
+            if (CheckGameStatus())
+            {
+                lblStatus.Text = $"Игрок {currentPlayer} выиграл!";
+                gameActive = false; 
+            }
+            else if (IsBoardFull())
+            {
+                lblStatus.Text = "Ничья!";
+                gameActive = false; 
+            }
+            else
+            {
+                currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+            }
+        }
+
+        private bool CheckGameStatus()
+        {
+            // Проверка строк, столбцов и диагоналей
+            for (int i = 0; i < 3; i++)
+            {
+                if ((gameBoard[i, 0] == currentPlayer && gameBoard[i, 1] == currentPlayer && gameBoard[i, 2] == currentPlayer) ||
+                    (gameBoard[0, i] == currentPlayer && gameBoard[1, i] == currentPlayer && gameBoard[2, i] == currentPlayer))
+                {
+                    return true;
+                }
+            }
+            // Проверка диагоналей
+            if ((gameBoard[0, 0] == currentPlayer && gameBoard[1, 1] == currentPlayer && gameBoard[2, 2] == currentPlayer) ||
+                (gameBoard[0, 2] == currentPlayer && gameBoard[1, 1] == currentPlayer && gameBoard[2, 0] == currentPlayer))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsBoardFull()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (gameBoard[i, j] == '\0') 
+                        return false;
+                }
+            }
+            return true; 
+        }
+
+        private void BtnRestart_Click(object sender, EventArgs e)
+        {
+            currentPlayer = 'X';
+            gameActive = true;
+            gameBoard = new char[3, 3]; 
+            lblStatus.Text = ""; 
+
+            foreach (Control control in tableLayoutPanel.Controls)
+            {
+                if (control is Button button)
+                {
+                    button.Text = ""; 
+                }
+            }
+        }
+
+        private void BtnDraw_Click(object sender, EventArgs e)
+        {
+            if (gameActive)
+            {
+                lblStatus.Text = "Игра завершена. Предложена ничья.";
+                gameActive = false; 
+            }
+            else
+            {
+                MessageBox.Show("Игра уже завершена!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void BtnResign_Click(object sender, EventArgs e)
+        {
+            if (gameActive)
+            {
+                lblStatus.Text = $"Игрок {currentPlayer} признал поражение. Победитель: {(currentPlayer == 'X' ? 'O' : 'X')}";
+                gameActive = false; 
+            }
+            else
+            {
+                MessageBox.Show("Игра уже завершена!", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+    }
+
+}
